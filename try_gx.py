0288bbe135b5eef8230e17481128ae67c4ff7fe9
pornpanida <67130155@dpu.ac.th>
2025-03-21 09:31:03 +0000

    Implement data quality

---
+++ try_gx.py
@@ -0,0 +1,18 @@
+import json
+
+import great_expectations as gx
+import pandas as pd
+
+
+def _validate_data():
+    columns = ["NewConfirmed", "Date"]
+    df = pd.read_csv("data.csv", names=columns)
+
+    valid_df = df[(df.NewConfirmed >= 0) & (df.NewConfirmed <= 322315)]
+    dq = len(valid_df) / len(df)
+    print(dq)
+
+    assert len(valid_df) == len(df)
+
+
+_validate_data()
